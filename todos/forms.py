c5576bcc1d63225b27aff72f007e368905528a1d
Ivinzz10 <ivinsuned10@gmail.com>
2025-12-17 22:30:39 +0530

    Initial commit: todos app with CRUD and styled UI

---
+++ todos/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Todo
+
+class TodoForm(forms.ModelForm):
+    class Meta:
+        model = Todo
+        fields = ['subject', 'notes']
+        widgets = {
+            'subject': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter subject'
+            }),
+            'notes': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 4,
+                'placeholder': 'Enter notes'
+            }),
+        }
---
+++ todos/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Todo
+from .forms import TodoForm
+
+def todo_list(request):
+    todos = Todo.objects.all()
+    return render(request, 'todos/todo_list.html', {'todos': todos})
+
+def todo_create(request):
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todo_list')
+    else:
+        form = TodoForm()
+    return render(request, 'todos/todo_form.html', {'form': form})
+
+def todo_update(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    if request.method == 'POST':
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+            return redirect('todo_list')
+    else:
+        form = TodoForm(instance=todo)
+    return render(request, 'todos/todo_form.html', {'form': form})
+
+def todo_delete(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    todo.delete()
+    return redirect('todo_list')
+
+
+
